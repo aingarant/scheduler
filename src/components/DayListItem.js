86a01886a5d739a8dc55810b470b874daf47f530
Aingaran Thirunadarajah <aingaran.thirunadarajah@telus.com>
2023-01-10 09:50:02 -0500

    Completed Building the DayListItem

--- src/components/DayListItem.js
+++ src/components/DayListItem.js
@@ -1,16 +1,15 @@
-import React from "react";
+import React, { useState } from "react";
 
 const DayListItem = (props) => {
-  const {dayName, daysRemaning, daySelected} = props;
 
-  const handleSelect  = () =>
-  {
 
-  }
+  const [day, setDay] = useState('')
+
+
   return (
-    <li>
-      <h2 className="text--regular">Day Name</h2>
-      <h3 className="text--light">X spots remaining</h3>
+    <li onClick={() => props.setDay(props.name)}>
+      <h2 className="text--regular">{props.name}</h2>
+      <h3 className="text--light">{props.spots} spots remaining</h3>
     </li>
   );
 };
