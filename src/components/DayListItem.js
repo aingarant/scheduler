5fe3f967c7e8f0548a85294b7fc3fb424585eedc
Aingaran Thirunadarajah <aingaran.thirunadarajah@telus.com>
2023-01-10 10:07:27 -0500

    Completed Testing the DayListItem

--- src/components/DayListItem.js
+++ src/components/DayListItem.js
@@ -6,16 +6,29 @@ const DayListItem = (props) => {
 
   const dayListItemClass = classNames("day-list__item", {
     "day-list__item--selected": props.selected,
-    "day-list__item--full": props.full <=0
+    "day-list__item--full": props.full <= 0
   });
 
-  const [day, setDay] = useState('')
+  const [day, setDay] = useState('');
+
+  const formatSpots = (spots) => {
+
+    if (spots === 0) {
+      return "no spots remaining"
+    }
+
+    if (spots === 1) {
+      return "1 spot remaining"
+    }
+
+    return `${spots} spots remaining`
+  }
 
 
   return (
     <li onClick={() => props.setDay(props.name)} className={dayListItemClass}>
       <h2>{props.name}</h2>
-      <h3>{props.spots} spots remaining</h3>
+      <h3>{formatSpots(props.spots)}</h3>
     </li>
   );
 };
