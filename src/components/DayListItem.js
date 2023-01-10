7c8ada6249db3ba48fae80084bde60c6947c1148
Aingaran Thirunadarajah <aingaran.thirunadarajah@telus.com>
2023-01-10 10:00:07 -0500

    Completed Styling the DayListItem

--- src/components/DayListItem.js
+++ src/components/DayListItem.js
@@ -1,15 +1,21 @@
 import React, { useState } from "react";
-
+import classNames from "classnames";
+import "./DayListItem.scss"
 const DayListItem = (props) => {
 
+
+  const dayListItemClass = classNames("day-list__item", {
+    "day-list__item--selected": props.selected,
+    "day-list__item--full": props.full <=0
+  });
 
   const [day, setDay] = useState('')
 
 
   return (
-    <li onClick={() => props.setDay(props.name)}>
-      <h2 className="text--regular">{props.name}</h2>
-      <h3 className="text--light">{props.spots} spots remaining</h3>
+    <li onClick={() => props.setDay(props.name)} className={dayListItemClass}>
+      <h2>{props.name}</h2>
+      <h3>{props.spots} spots remaining</h3>
     </li>
   );
 };
