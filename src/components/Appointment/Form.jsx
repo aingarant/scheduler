a5c20c4af835b01e26b458c99f7b74c7e795ef3b
Aingaran Thirunadarajah <aingaran.thirunadarajah@telus.com>
2023-01-10 20:06:34 -0500

    Completed Props and State in the Appointment Form

--- src/components/Appointment/Form.jsx
+++ src/components/Appointment/Form.jsx
@@ -1,9 +1,54 @@
-import React from 'react'
+import React, { useState } from "react";
+import Button from "components/Button";
+import InterviewerList from "components/InterviewerList";
 
-const AppointmentForm = () => {
+const Form = (props) => {
+  const [student, setStudent] = useState(props.student || "");
+  const [interviewer, setInterviewer] = useState(props.interviewer || null);
+
+  const reset = () => {
+    setStudent("");
+    setInterviewer(null);
+  };
+
+  const cancel = () => {
+    reset();
+    props.onCancel();
+  };
+
   return (
-    <div>AppointmentForm</div>
-  )
-}
+    <main className="appointment__card appointment__card--create">
+      <section className="appointment__card-left">
+        <form onSubmit={e => e.preventDefault()} autoComplete="off">
+          <input
+            className="appointment__create-input text--semi-bold"
+            name="name"
+            type="text"
+            placeholder="Enter Student Name"
+            value={student}
+            onChange={(e) => {
+              setStudent(e.target.value);
+            }}
+          />
+        </form>
+        <InterviewerList
+          interviewers={props.interviewers}
+          value={interviewer}
+          onChange={setInterviewer}
+        />
+      </section>
+      <section className="appointment__card-right">
+        <section className="appointment__actions">
+          <Button danger onClick={cancel}>
+            Cancel
+          </Button>
+          <Button confirm onClick={props.onSave}>
+            Save
+          </Button>
+        </section>
+      </section>
+    </main>
+  );
+};
 
-export default AppointmentForm
+export default Form;
