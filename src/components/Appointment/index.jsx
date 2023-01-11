bb1dc043c8ac238b97c6990a6029636d01d1d200
Aingaran Thirunadarajah <aingaran.thirunadarajah@telus.com>
2023-01-10 20:28:13 -0500

    Completed Displaying Appointments

--- src/components/Appointment/index.jsx
+++ src/components/Appointment/index.jsx
@@ -1,10 +1,30 @@
-import React from 'react'
-import "./styles.scss"
-import Show from './Show'
-const Appointment = () => {
-  return (
-<article className="appointment"></article>
-  )
-}
+import React from "react";
+import "./styles.scss";
+import Header from "./Header";
+import Show from "./Show";
+import Empty from "./Emtpy";
+import Form from "./Form";
+import Confirm from "./Confirm";
+import Status from "./Status";
+import Error from "./Error";
 
-export default Appointment
+const Appointment = (props) => {
+  return (
+    <article className="appointment">
+      <Header time={props.time} />
+
+      {props.interview ? (
+        <Show
+          student={props.student}
+          interviewer={props.interviewer}
+          onEdit={props.onEdit}
+          onDelete={props.onDelete}
+        />
+      ) : (
+        <Empty />
+      )}
+    </article>
+  );
+};
+
+export default Appointment;
