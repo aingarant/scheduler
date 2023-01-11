75c4f8b2bc2a01b7260558e80ae6e45139b2e029
Aingaran Thirunadarajah <aingaran.thirunadarajah@telus.com>
2023-01-10 20:46:28 -0500

    Completed the Schedule and fixed DaylistItem Component

--- src/components/DayListItem.jsx
+++ src/components/DayListItem.jsx
@@ -4,9 +4,13 @@ import "./DayListItem.scss"
 
 const DayListItem = (props) => {
 
+  console.log("*******************")
+  console.log(props)
+
+
   const dayListItemClass = classNames("day-list__item", {
     "day-list__item--selected": props.selected,
-    "day-list__item--full": props.full <= 0
+    "day-list__item--full": props.spots <= 0
   });
 
   const formatSpots = (spots) => {
@@ -24,7 +28,7 @@ const DayListItem = (props) => {
 
 
   return (
-    <li onClick={() => props.onChange(props.name)} className={dayListItemClass}  selected={props.selected}>
+    <li onClick={() => props.setDay(props.name)} className={dayListItemClass}  selected={props.selected}>
       <h2>{props.name}</h2>
       <h3>{formatSpots(props.spots)}</h3>
     </li>
