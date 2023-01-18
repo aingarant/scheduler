b728a644edccbe1103a4f9dc99bc1f12b89fb163
Aingaran Thirunadarajah <aingaran.thirunadarajah@telus.com>
2023-01-18 10:44:12 -0500

    Completed 3 cyrpress tests.. Add, edit, delete appointments

--- cypress/integration/appointments.spec.js
+++ cypress/integration/appointments.spec.js
@@ -9,28 +9,33 @@ describe("Apointment", () => {
     cy.contains("Monday");
   });
 
-  // it("should book an interview", () => {
-  //   cy.get("[alt=Add]")
-  //     .first()
-  //     .click()
-  //     .get("[data-testid=student-name-input]")
-  //     .type("Aingaran Thirunadarajah")
-  //     .get("[alt='Sylvia Palmer']")
-  //     .click();
-  //   cy.contains("Save").click();
-  //   cy.contains(".appointment__card--show", "Aingaran Thirunadarajah");
-  //   cy.contains(".appointment__card--show", "Sylvia Palmer");
-  // });
+  it("should book an interview", () => {
+    cy.get("[alt=Add]")
+      .first()
+      .click()
+      .get("[data-testid=student-name-input]")
+      .type("Aingaran Thirunadarajah")
+      .get("[alt='Sylvia Palmer']")
+      .click();
+    cy.contains("Save").click();
+    cy.contains(".appointment__card--show", "Aingaran Thirunadarajah");
+    cy.contains(".appointment__card--show", "Sylvia Palmer");
+  });
 
-  // it("should edit the interview", () => {
-  //   cy
-  //   .contains(".appointment__card--show", "Archie Cohen")
-  //   .trigger('mouseover')
-  //   cy.get("[alt=Edit]").should('be.visible')
-  // });
+  it("should edit the interview", () => {
+    cy.get("[alt='Edit']").first().click({ force: true })
+      .get("[data-testid=student-name-input]")
+      .clear()
+      .type("Aingaran Thirunadarajah")
+      .get("[alt='Sylvia Palmer']")
+      .click()
+    cy.contains("Save").click()
+    cy.contains(".appointment__card--show", "Aingaran Thirunadarajah")
+    cy.contains(".appointment__card--show", "Sylvia Palmer");
+  });
 
   it("should cancel an interview", () => {
-    cy.get("[alt='Delete']").first().click();
+    cy.get("[alt='Delete']").first().click({ force: true });
     cy.contains(/Confirm/i).click();
     cy.contains(/Deleting/i).should("not.exist");
     cy.contains(".appointment__card--show", "Archie Cohen").should("not.exist");
