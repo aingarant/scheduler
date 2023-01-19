2b38d24d308e0d3c926df741b385a4f51a13c136
Aingaran Thirunadarajah <aingaran.thirunadarajah@telus.com>
2023-01-18 23:44:43 -0500

    Added Comments

--- src/hooks/useApplicationData.js
+++ src/hooks/useApplicationData.js
@@ -2,10 +2,14 @@ import { useEffect, useReducer } from "react";
 import axios from "axios";
 import { INITIAL_STATE, appReducer } from "reducers";
 
-
 const useApplicationData = () => {
+  // Use Reducer to set intiial state.
   const [state, dispatch] = useReducer(appReducer, INITIAL_STATE);
+
+  // Function to update selected day state via Reducer.
   const setDay = day => dispatch({ type: "SET_DAY", payload: { day: day } });
+
+  // Update Spots Function.
   const updateSpots = (appointments) => {
     const dayObj = state.days.find((item) => item.name === state.day);
     const spots = dayObj.appointments.filter(
@@ -15,6 +19,7 @@ const useApplicationData = () => {
     return state.days.map((d) => (d.name === state.day ? day : d));
   };
 
+  // Book Interview Function
   const bookInterview = (id, interview) => {
     const appointment = {
       ...state.appointments[id],
@@ -32,6 +37,7 @@ const useApplicationData = () => {
       });
   };
 
+  // Cancel Interview Function
   const cancelInterview = (id) => {
     const appointment = {
       ...state.appointments[id],
@@ -48,7 +54,7 @@ const useApplicationData = () => {
       dispatch({ type: "SET_APPOINTMENT", payload: { appointments, days } })
     });
   };
-
+  // Use Effect to Fetch Data from API on Initial Load and set App data state via Reducer Dispatch
   useEffect(() => {
     Promise.all([
       axios.get("/api/appointments"),
